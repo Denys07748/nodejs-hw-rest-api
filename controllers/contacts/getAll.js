2fa4df87c1b2fee675d7f4f5f4c57c58849928bd
Denys07748 <denis07748@gmail.com>
2023-06-27 23:56:46 +0300

    fix ctrl.getAll

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -6,7 +6,7 @@ const getAll = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, 'createAt updateAt', {
+  const result = await Contact.find({ owner }, '', {
     skip,
     limit,
   }).populate('owner', 'email');
